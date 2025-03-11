6a4d160d0e71e30c56a90a03dc73f56b84700678
Manuk-Han <aksdnr507@gmail.com>
2025-03-11 16:13:25 +0900

    api gateway

---
+++ src/main/java/com/example/apigateway/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.apigateway;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/**").permitAll()
+                )
+                .build();
+    }
+}
