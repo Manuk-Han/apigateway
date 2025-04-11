41a5cdb49f504472f6af2f67aaa79219be24bc46
Manuk-Han <aksdnr507@gmail.com>
2025-04-11 17:26:40 +0900

    add problem create

--- src/main/java/com/example/apigateway/service/ProblemService.java
+++ src/main/java/com/example/apigateway/service/ProblemService.java
@@ -1,5 +1,15 @@
 package com.example.apigateway.service;
 
+import com.example.apigateway.common.exception.CustomException;
+import com.example.apigateway.common.exception.CustomResponseException;
+import com.example.apigateway.entity.Course;
+import com.example.apigateway.entity.Problem;
+import com.example.apigateway.entity.ProblemBank;
+import com.example.apigateway.entity.User;
+import com.example.apigateway.form.problem.ProblemCreateForm;
+import com.example.apigateway.repository.CourseRepository;
+import com.example.apigateway.repository.ProblemBankRepository;
+import com.example.apigateway.repository.ProblemRepository;
 import com.example.apigateway.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
@@ -12,5 +22,37 @@ import org.springframework.stereotype.Service;
 public class ProblemService {
     private final UserRepository userRepository;
 
+    private final CourseRepository courseRepository;
 
+    private final ProblemBankRepository problemBankRepository;
+
+    private final ProblemRepository problemRepository;
+
+    public Long createProblem(Long userId, String courseUUid, ProblemCreateForm problemCreateForm) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(CustomResponseException.NOT_FOUND_ACCOUNT));
+
+        Course course = courseRepository.findCourseByCourseUUid(courseUUid)
+                .orElseThrow(() -> new CustomException(CustomResponseException.NOT_FOUND_COURSE));
+
+        validateCourseOwner(user, course);
+
+        if (problemBankRepository.existsByCourse(course)) {
+            ProblemBank problemBank = problemBankRepository.findByCourse(course);
+
+            Problem problem = Problem.builder()
+                    .problemTitle(problemCreateForm.getProblemTitle())
+                    .problemDescription(problemCreateForm.getProblemDescription())
+//                    .problemRestriction(problemCreateForm.getProblemRestriction())
+                    .build();
+        }
+
+        return null;
+    }
+
+    private void validateCourseOwner(User user, Course course) {
+        if (!course.getOwner().equals(user)) {
+            throw new CustomException(CustomResponseException.FORBIDDEN);
+        }
+    }
 }
