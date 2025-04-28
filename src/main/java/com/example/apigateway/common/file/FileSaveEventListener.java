6cf36ac87770ec2336a5df3fb8dac44a0253c6f1
Manuk-Han <aksdnr507@gmail.com>
2025-04-28 12:02:08 +0900

    update FileSaveEventListener

--- src/main/java/com/example/apigateway/common/file/FileSaveEventListener.java
+++ src/main/java/com/example/apigateway/common/file/FileSaveEventListener.java
@@ -4,6 +4,8 @@ import com.example.apigateway.common.exception.CustomException;
 import com.example.apigateway.common.exception.CustomResponseException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -14,25 +16,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Profile("class")
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FileSaveEventListener {
+    @Value("${file.testcase.path}")
+    private static String PATH_PREFIX;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void signatureFileSaveEvent(TestcaseFileSaveEvent testcase) {
         try {
-            saveStringToFile(testcase.getInputContent(), "/problem/" + testcase.getProblemId() + "/testcase/input" + testcase.getNum() + ".txt");
-            saveStringToFile(testcase.getOutputContent(), "/problem/" + testcase.getProblemId() + "/testcase/output" + testcase.getNum() + ".txt");
+            saveStringToFile(testcase.getInputContent(), testcase.getProblemId(), testcase.getNum());
+            saveStringToFile(testcase.getOutputContent(), testcase.getProblemId(), testcase.getNum());
         } catch (IOException e) {
             log.error("Error saving user signature file: {}", e.getMessage());
             throw new CustomException(CustomResponseException.INVALID_EXCEL_FILE);
         }
     }
 
-    public static void saveStringToFile(String content, String filePath) throws IOException {
-        Path path = Paths.get(filePath);
+    public static void saveStringToFile(String content, Long problemId, int num) throws IOException {
+        Path path = Paths.get(PATH_PREFIX + "/problem/" + problemId + "/testcase/input" + num + ".txt");
         Files.write(path, content.getBytes());
     }
 }
